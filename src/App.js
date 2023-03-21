0719b38fccc0b0188dded3efb6d880d7ccebd240
Msimusi <120442998+Msimusi@users.noreply.github.com>
2023-03-21 18:58:24 +0900

    adad

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/abot-us">
-          <h1>Hello</h1>
-        </Route>
-        <Route path={process.env.PUBLIC_URL + "/movie/:id"}>
-          <Detail />
-        </Route>
-        <Route path={process.env.PUBLIC_URL + "/"}>
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+      </Routes>
     </Router>
   );
 }
