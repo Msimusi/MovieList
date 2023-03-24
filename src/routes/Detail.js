d916b330d689ba927abea667eb641c9a77309e08
Msimusi <120442998+Msimusi@users.noreply.github.com>
2023-03-24 10:57:58 +0900

    done

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,12 +3,14 @@ import { Link, useParams } from "react-router-dom";
 function Detail() {
   const [movie, setMovie] = useState({});
   const { id } = useParams();
+  
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     setMovie(json.data.movie);
   };
+  
   useEffect(() => {
     getMovie();
   }, []);
